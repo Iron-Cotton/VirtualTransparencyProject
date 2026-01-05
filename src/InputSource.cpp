76ef0cf1801c678ad1677a84d602624aedd1ee29
Iron-Cotton <alexander.112100@gmail.com>
2026-01-06 04:13:46 +0900

    いったん全てコメントアウト

--- src/InputSource.cpp
+++ src/InputSource.cpp
@@ -3,26 +3,26 @@
 
 // --- RealSenseSource の実装 ---
 
-bool RealSenseSource::initialize() {
-    std::cout << "[RealSense] Initializing..." << std::endl;
-    // TODO: RealSenseパイプラインの開始処理をここに書く
-    return true;
-}
+// bool RealSenseSource::initialize() {
+//     std::cout << "[RealSense] Initializing..." << std::endl;
+//     // TODO: RealSenseパイプラインの開始処理をここに書く
+//     return true;
+// }
 
-bool RealSenseSource::update(PointCloudData& outData) {
-    // TODO: フレームを取得し、outDataに詰める処理をここに書く
-    return true;
-}
+// bool RealSenseSource::update(PointCloudData& outData) {
+//     // TODO: フレームを取得し、outDataに詰める処理をここに書く
+//     return true;
+// }
 
-// --- FileSource の実装 ---
+// // --- FileSource の実装 ---
 
-bool FileSource::initialize() {
-    std::cout << "[File] Initializing video source..." << std::endl;
-    // TODO: 動画ファイルのロード処理
-    return true;
-}
+// bool FileSource::initialize() {
+//     std::cout << "[File] Initializing video source..." << std::endl;
+//     // TODO: 動画ファイルのロード処理
+//     return true;
+// }
 
-bool FileSource::update(PointCloudData& outData) {
-    // TODO: 次のフレームを読み込む処理
-    return true;
-}
+// bool FileSource::update(PointCloudData& outData) {
+//     // TODO: 次のフレームを読み込む処理
+//     return true;
+// }
