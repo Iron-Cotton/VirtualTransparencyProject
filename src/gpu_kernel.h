db558d6d644c5172555da73c2065882eb524a4bc
Iron-Cotton <alexander.112100@gmail.com>
2026-01-07 11:57:26 +0900

    ラッパー関数に引数timingsを追加

--- src/gpu_kernel.h
+++ src/gpu_kernel.h
@@ -1,12 +1,13 @@
 #pragma once
-#include "Common.h"
+#include "Common.h" // ProcessTimingsのために必要
 #include <cuda_runtime.h>
 
-// 引数にボクセルバッファ(r, g, b, cnt)を追加
+// カーネルラッパー関数
 void runReconstructionKernel(
     PointCloudData& data, 
     const AppConfig& config, 
     unsigned int* d_r, unsigned int* d_g, unsigned int* d_b, unsigned int* d_cnt,
     uchar4* d_output, 
-    int width, int height
+    int width, int height,
+    ProcessTimings& timings // ★追加
 );
